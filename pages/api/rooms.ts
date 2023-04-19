2954138ef8e038f7232634075991b0ae48d3b62c
UfukCemDELICE <ufukcemdelice02@gmail.com>
2023-04-20 00:33:03 +0300

    rooms created

--- pages/api/rooms.ts
+++ pages/api/rooms.ts
@@ -17,12 +17,26 @@ export default async function handler(
   switch (method) {
     case 'GET':
       try {
-        const rooms = await Room.find({});
+        const rooms = await Room.aggregate([
+          { $match: {status: "waiting"}},
+          { $sample: { size: 1 } }
+        ]);
+        if (rooms.length > 0){
+          const roomId = rooms[0]._id;
+          await Room.findByIdAndUpdate(roomId, {
+            status: "chatting",
+          });
+        }
         res.status(200).json(rooms);
       } catch (error) {
         res.status(400).json((error as any).message);
       } 
       break;
+    case "POST":
+      const room = await Room.create({
+        status: "waiting",
+      });
+      res.status(200).json(room);
     default:
       res.status(400).json("Bad request");
       break;
